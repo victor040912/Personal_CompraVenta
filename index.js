fb5c204683b97ab7c468c5a642fb483f7a7c2b97
Victor Alonso Quispe Quiroz <victorquispe@HIRAOKA.LOCAL>
2025-08-22 12:37:25 -0500

    correcion de gpt

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('#emp_id').select2();
     $('#suc_id').select2();
 
-    $.post("/Personal_CompraVenta/controller/empresa.php?op=combo",{com_id:1},function(data){
+    $.post("controller/empresa.php?op=combo",{com_id:1},function(data){
         $("#emp_id").html(data);
     });
 
@@ -11,7 +11,7 @@ $(document).ready(function(){
         $("emp_id").each(function(){
             emp_id = $(this).val();
            
-            $.post("/Personal_CompraVenta/controller/sucursal.php?op=combo",{emp_id:emp_id},function(data){
+            $.post("controller/sucursal.php?op=combo",{emp_id:emp_id},function(data){
                  $("#suc_id").html(data);
             });
         });
